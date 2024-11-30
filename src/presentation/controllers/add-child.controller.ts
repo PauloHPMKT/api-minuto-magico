f6c639e9750f841792d48db918970b88dabf0c2b
Paulo Sergio <pauloserg861@gmail.com>
2024-11-30 17:22:52 -0300

    chore: add and configure jest

--- src/presentation/controllers/add-child.controller.ts
+++ src/presentation/controllers/add-child.controller.ts
@@ -2,7 +2,7 @@ import { HttpResponse } from "../http/httpReponse"
 import { HttpRequest } from "../http/httpRequest"
 import { Controller } from "../protocols/Controller"
 
-export class AddChildController implements Controller{
+export class AddChildController implements Controller {
   async handle(params: HttpRequest): Promise<HttpResponse> {
     return {
       statusCode: 200,
---
+++ src/presentation/controllers/add-child.spec.ts
@@ -0,0 +1,5 @@
+describe('AddChild Controller', () => {
+  test('should return 200 if valid data is provided', () => {
+    expect(1).toBe(1)
+  })
+})
