be1e345745233852dc550a07113e106a0fd21391
Paulo Sergio <pauloserg861@gmail.com>
2024-11-30 22:08:48 -0300

    test: ensure AddChildController returns 400 if no name is provided

--- src/presentation/controllers/add-child.spec.ts
+++ src/presentation/controllers/add-child.spec.ts
@@ -5,4 +5,15 @@ describe('AddChild Controller', () => {
     const sut = new AddChildController()
     expect(sut).toBeDefined()
   })
+
+  it('Should return 400 if no name is provided', async () => {
+    const sut = new AddChildController()
+    const httpRequest = {
+      body: {
+        totalMinutes: 10,
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
