f4e7d686c9603f17ede221ccaa42469694c312fe
Paulo Sergio <pauloserg861@gmail.com>
2024-12-03 21:36:57 -0300

    refactor: add fields validation to a loop verification

--- src/presentation/controller/add-child.ts
+++ src/presentation/controller/add-child.ts
@@ -2,17 +2,13 @@ import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class AddChildController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
-    }
-
-    if (!httpRequest.body.totalMinutes) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: totalMinutes')
+    const requiredFields = ['name', 'totalMinutes']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`)
+        }
       }
     }
   }
