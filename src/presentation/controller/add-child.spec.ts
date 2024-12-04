49021a52cbbfcadb5407b991d13bd579822a487b
Paulo Sergio <pauloserg861@gmail.com>
2024-12-03 21:30:11 -0300

    test: ensure AddChildController throws an error if no totalMinutes and no name is provided

--- src/presentation/controller/add-child.spec.ts
+++ src/presentation/controller/add-child.spec.ts
@@ -1,13 +1,17 @@
 import { AddChildController } from "./add-child"
 
+const makeSut = (): AddChildController => {
+  return new AddChildController()
+}
+
 describe('AddChildController', () => {
   it('Should be defined', () => {
-    const sut = new AddChildController()
+    const sut = makeSut()
     expect(sut).toBeDefined()
   })
 
   it('Should AddChildController return 400 if no name is provided', () => {
-    const sut = new AddChildController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         totalMinutes: 10,
@@ -15,10 +19,11 @@ describe('AddChildController', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 
   it('Should AddChildController return 400 if no totalMinutes is provided', () => {
-    const sut = new AddChildController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -26,5 +31,6 @@ describe('AddChildController', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: totalMinutes'))
   })
 })
