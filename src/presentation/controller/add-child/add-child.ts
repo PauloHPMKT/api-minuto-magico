2667576964ccc43b097dd6368540c51eed72a5eb
Paulo Sergio <pauloserg861@gmail.com>
2024-12-17 10:50:48 -0300

    refactor: move httpResponse to a helper method

--- src/presentation/controller/add-child/add-child.ts
+++ src/presentation/controller/add-child/add-child.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../../errors/invalid-param.error';
 import { MissingParamError } from '../../errors/missing-param-error';
+import { badRequest } from '../../helpers/http';
 import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
 
@@ -8,19 +9,13 @@ export class AddChildController implements Controller {
     const requiredFields = ['name', 'totalMinutes'];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError(field),
-        };
+        return badRequest(new MissingParamError(field));
       }
     }
 
     const { name, totalMinutes } = httpRequest.body;
     if (isNaN(totalMinutes)) {
-      return {
-        statusCode: 400,
-        body: new InvalidParamError('totalMinutes'),
-      };
+      return badRequest(new InvalidParamError('totalMinutes'));
     }
   }
 }
