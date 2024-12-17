6a2ec2291474efa154e9977842b2f6750176a970
Paulo Sergio <pauloserg861@gmail.com>
2024-12-17 10:49:05 -0300

    refactor: implement method signature to Controller

--- src/presentation/controller/add-child/add-child.ts
+++ src/presentation/controller/add-child/add-child.ts
@@ -1,8 +1,10 @@
 import { InvalidParamError } from '../../errors/invalid-param.error';
 import { MissingParamError } from '../../errors/missing-param-error';
+import { Controller } from '../../protocols/controller';
+import { HttpRequest, HttpResponse } from '../../protocols/http';
 
-export class AddChildController {
-  handle(httpRequest: any): any {
+export class AddChildController implements Controller {
+  handle(httpRequest: HttpRequest): any {
     const requiredFields = ['name', 'totalMinutes'];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
