c3866705cfbbebfb7caed9834e3a3f53ad14ab59
Paulo Sergio <pauloserg861@gmail.com>
2024-12-19 09:37:55 -0300

    test: ensure AddChildRepository is called with correct values

--- src/data/usecases/db-add-child.spec.ts
+++ src/data/usecases/db-add-child.spec.ts
@@ -1,13 +1,58 @@
+import { Child } from '../../domain/entities/Child';
+import { AddChildModel } from '../../domain/models/add-child';
+import { AddChildRepository } from '../protocols/add-child-repository';
 import { DbAddChild } from './db-add-child';
 
-const makeSut = (): DbAddChild => {
-  const sut = new DbAddChild();
-  return sut;
+const makeAddChildRepository = (): AddChildRepository => {
+  class AddChildRepositoryStub implements AddChildRepository {
+    async add(child: AddChildModel.Params): Promise<Child> {
+      const fakeChild = {
+        id: 'valid_id',
+        name: 'valid_name',
+        totalMinutes: 10,
+        entryTime: new Date(),
+        exitTime: null as any,
+        createdAt: new Date(),
+        updatedAt: null as any,
+      };
+      return new Promise((resolve) => resolve(fakeChild));
+    }
+  }
+
+  return new AddChildRepositoryStub();
 };
+
+const makeSut = (): SutTypes => {
+  const addChildRepositoryStub = makeAddChildRepository();
+  const sut = new DbAddChild(addChildRepositoryStub);
+  return {
+    sut,
+    addChildRepositoryStub,
+  };
+};
+
+interface SutTypes {
+  sut: DbAddChild;
+  addChildRepositoryStub: AddChildRepository;
+}
 
 describe('DbAddChild', () => {
   it('Should be defined', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('Should call AddChildRepository with correct values', async () => {
+    const { sut, addChildRepositoryStub } = makeSut();
+    const addSpy = jest.spyOn(addChildRepositoryStub, 'add');
+    const childData = {
+      name: 'valid_name',
+      totalMinutes: 10,
+    };
+    await sut.add(childData);
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      totalMinutes: 10,
+    });
   });
 });
