2a6c6a3bbad508135488e458e73e6ed88aaaba4f
Paulo Sergio <pauloserg861@gmail.com>
2024-12-21 13:53:49 -0300

    feat: ensure content-type receive correct application/json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/body-parser';
+import { contentType } from '../middlewares/content-type';
 
 export default (app: Express): void => {
-  const middlewares = [bodyParser];
+  const middlewares = [bodyParser, contentType];
   middlewares.forEach((middleware) => app.use(middleware));
 };
