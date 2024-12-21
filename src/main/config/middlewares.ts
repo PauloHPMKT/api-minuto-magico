0531a89ba43dfd5456a8cbbcd34512f9e0c9edec
Paulo Sergio <pauloserg861@gmail.com>
2024-12-21 14:02:48 -0300

    feat: enable cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/body-parser';
 import { contentType } from '../middlewares/content-type';
+import { cors } from '../middlewares/cors';
 
 export default (app: Express): void => {
-  const middlewares = [bodyParser, contentType];
+  const middlewares = [bodyParser, contentType, cors];
   middlewares.forEach((middleware) => app.use(middleware));
 };
