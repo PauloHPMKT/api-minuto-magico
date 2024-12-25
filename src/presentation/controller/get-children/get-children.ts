586939d97e4af5f6ab41378969e4029bec9ae54c
Paulo Sergio <pauloserg861@gmail.com>
2024-12-25 18:23:02 -0300

    feat: ensure GetChildrenController returns 200 on success

--- src/presentation/controller/get-children/get-children.ts
+++ src/presentation/controller/get-children/get-children.ts
@@ -1,1 +1,13 @@
-export class GetChildrenController {}
+import { ok } from '../../helpers/http';
+import { Controller } from '../../protocols/controller';
+import { HttpResponse } from '../../protocols/http';
+
+export class GetChildrenController implements Controller {
+  async handle(): Promise<HttpResponse> {
+    try {
+      return ok([]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
