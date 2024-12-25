6186cd16d96776091ceac98a42359dfa254d9cac
Paulo Sergio <pauloserg861@gmail.com>
2024-12-25 18:23:20 -0300

    test: ensure GetChildrenController returns 200 on success

--- src/presentation/controller/get-children/get-children.spec.ts
+++ src/presentation/controller/get-children/get-children.spec.ts
@@ -10,4 +10,10 @@ describe('GetChildrenController', () => {
     const sut = makeSut();
     expect(sut).toBeDefined();
   });
+
+  it('Should return 200 on success', async () => {
+    const sut = makeSut();
+    const HttpResponse = await sut.handle();
+    expect(HttpResponse.statusCode).toBe(200);
+  });
 });
