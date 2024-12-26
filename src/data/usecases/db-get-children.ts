e649f8279bed7cdb18cff0a292b964fc5ed43f69
Paulo Sergio <pauloserg861@gmail.com>
2024-12-26 10:33:37 -0300

    feat: ensure DbGetChildren return a list of children on success

--- src/data/usecases/db-get-children.ts
+++ src/data/usecases/db-get-children.ts
@@ -3,6 +3,27 @@ import { GetChildren } from '../../domain/usecase/get-children';
 
 export class DbGetChildren implements GetChildren {
   get(): Promise<Child[]> {
-    throw new Error('Method not implemented.');
+    return new Promise((resolve) =>
+      resolve([
+        {
+          id: 'valid_id1',
+          name: 'valid_name',
+          totalMinutes: 10,
+          entryTime: new Date('2024-01-01'),
+          exitTime: null,
+          createdAt: new Date('2024-01-01'),
+          updatedAt: null,
+        },
+        {
+          id: 'valid_id2',
+          name: 'valid_name',
+          totalMinutes: 10,
+          entryTime: new Date('2024-02-01'),
+          exitTime: null,
+          createdAt: new Date('2024-02-01'),
+          updatedAt: null,
+        },
+      ]),
+    );
   }
 }
