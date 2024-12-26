9fd10b59fbb1d6598dce3b6f2731b5c67e27502e
Paulo Sergio <pauloserg861@gmail.com>
2024-12-26 11:35:06 -0300

    test: ensure ChildRepository returns a list of children

--- src/infra/db/mongodb/child-repository/child.spec.ts
+++ src/infra/db/mongodb/child-repository/child.spec.ts
@@ -1,18 +1,18 @@
+import { Child } from '../../../../domain/entities/Child';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { ChildRepository } from './child';
 
 describe('ChildRepository', () => {
+  let childCreated: Child[];
+
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string);
   });
 
   afterAll(async () => {
-    await MongoHelper.disconnect();
-  });
-
-  beforeEach(async () => {
     const childCollection = MongoHelper.getCollection('children');
     await childCollection.deleteMany({});
+    await MongoHelper.disconnect();
   });
 
   const makeSut = (): ChildRepository => {
@@ -29,6 +29,8 @@ describe('ChildRepository', () => {
       createdAt: new Date(),
       updatedAt: null as any,
     });
+    childCreated = [child];
+
     expect(child).toBeTruthy();
     expect(child.id).toBeTruthy();
     expect(child.name).toBe('valid_name');
@@ -37,5 +39,20 @@ describe('ChildRepository', () => {
     expect(child.exitTime).toBeNull();
     expect(child.createdAt).toBeInstanceOf(Date);
     expect(child.updatedAt).toBeNull();
+  });
+
+  it('Should return a list of children on success', async () => {
+    const sut = makeSut();
+    const children = await sut.get();
+
+    expect(children).toEqual(childCreated);
+    expect(children).toBeTruthy();
+    expect(children[0].id).toBeTruthy();
+    expect(children[0].name).toBe('valid_name');
+    expect(children[0].totalMinutes).toBe(10);
+    expect(children[0].entryTime).toBeInstanceOf(Date);
+    expect(children[0].exitTime).toBeNull();
+    expect(children[0].createdAt).toBeInstanceOf(Date);
+    expect(children[0].updatedAt).toBeNull();
   });
 });
