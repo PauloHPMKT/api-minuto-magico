f3c1eae274e4bce9cc94e29c94d1ba9e24b76f29
Paulo Sergio <pauloserg861@gmail.com>
2024-12-26 10:46:32 -0300

    feat: ensure DbGetChildren throws if no children are found

--- src/data/usecases/db-get-children.ts
+++ src/data/usecases/db-get-children.ts
@@ -6,28 +6,10 @@ export class DbGetChildren implements GetChildren {
   constructor(private readonly getChildrenRepository: GetChildrenRepository) {}
 
   async get(): Promise<Child[]> {
-    await this.getChildrenRepository.get();
-    return new Promise((resolve) =>
-      resolve([
-        {
-          id: 'valid_id1',
-          name: 'valid_name',
-          totalMinutes: 10,
-          entryTime: new Date('2024-01-01'),
-          exitTime: null,
-          createdAt: new Date('2024-01-01'),
-          updatedAt: null,
-        },
-        {
-          id: 'valid_id2',
-          name: 'valid_name',
-          totalMinutes: 10,
-          entryTime: new Date('2024-02-01'),
-          exitTime: null,
-          createdAt: new Date('2024-02-01'),
-          updatedAt: null,
-        },
-      ]),
-    );
+    const children = await this.getChildrenRepository.get();
+    if (!children.length) {
+      throw new Error('No children found');
+    }
+    return children;
   }
 }
