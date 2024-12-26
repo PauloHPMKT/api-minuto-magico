5514acc8a3cc2355be8b88432c61762bf4120e52
Paulo Sergio <pauloserg861@gmail.com>
2024-12-26 12:29:39 -0300

    test: ensure a GetChildren router return a list of children

--- src/main/routes/child-routes.test.ts
+++ src/main/routes/child-routes.test.ts
@@ -25,4 +25,29 @@ describe('Child Routes', () => {
       })
       .expect(201);
   });
+
+  it('should return a list of children on success', async () => {
+    await request(app)
+      .post('/api/children')
+      .send({
+        name: 'any_name',
+        totalMinutes: 10,
+      })
+      .expect(201);
+
+    const response = await request(app).get('/api/children').expect(200);
+    console.log(response.body);
+    expect(response.body.length).toBe(1);
+    expect(response.body).toEqual([
+      {
+        name: 'any_name',
+        totalMinutes: 10,
+        entryTime: expect.any(String),
+        exitTime: null,
+        createdAt: expect.any(String),
+        updatedAt: null,
+        id: expect.any(String),
+      },
+    ]);
+  });
 });
