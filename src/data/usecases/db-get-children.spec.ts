0de210870951ddd03039044ac73bda7ca417af12
Paulo Sergio <pauloserg861@gmail.com>
2024-12-26 10:34:20 -0300

    test: ensure DbGetChildren return a list of children on success

--- src/data/usecases/db-get-children.spec.ts
+++ src/data/usecases/db-get-children.spec.ts
@@ -16,4 +16,29 @@ describe('DbGetChildren', () => {
     const sut = makeSut();
     expect(sut).toBeDefined();
   });
+
+  it('Should return children on success', async () => {
+    const { sut } = makeSut();
+    const children = await sut.get();
+    expect(children).toEqual([
+      {
+        id: 'valid_id1',
+        name: 'valid_name',
+        totalMinutes: 10,
+        entryTime: new Date('2024-01-01'),
+        exitTime: null,
+        createdAt: new Date('2024-01-01'),
+        updatedAt: null,
+      },
+      {
+        id: 'valid_id2',
+        name: 'valid_name',
+        totalMinutes: 10,
+        entryTime: new Date('2024-02-01'),
+        exitTime: null,
+        createdAt: new Date('2024-02-01'),
+        updatedAt: null,
+      },
+    ]);
+  });
 });
